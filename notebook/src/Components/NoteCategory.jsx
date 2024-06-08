769b40788e9349b98ef181c16d85ec35acbaaf4f
harsh12chauhan <harshchauhan0225@gmail.com>
2024-06-08 13:54:41 +0530

    Created files and folder structure.

---
+++ notebook/src/Components/NoteCategory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteCategory = () => {
+  return (
+    <div>NoteCategory</div>
+  )
+}
+
+export default NoteCategory
---
+++ notebook/src/Pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home';
+import Notes from './Notes';
+
+export {
+    Home,Notes
+}
