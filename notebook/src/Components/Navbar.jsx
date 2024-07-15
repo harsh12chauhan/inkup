db25aa825c77555f592d3b2a956832986c3ddc0a
harsh12chauhan <harshchauhan0225@gmail.com>
2024-07-15 16:57:42 +0530

    created a logout button space in navbar.

--- notebook/src/Components/Navbar.jsx
+++ notebook/src/Components/Navbar.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-center items-center my-2 relative'>
+    <nav className='flex justify-center items-center mb-2 sticky top-0 bg-white shadow-sm'>
         <div>
-            <h2 className='text-gray-950 mx-4 text-xl font-bold hover:translate-x-4 hover:transition-all duration-1000 cursor-pointer '>Home</h2>
+            <h2 className='text-gray-950 mx-4 py-2 text-xl font-bold hover:translate-x-4 hover:transition-all duration-1000 cursor-pointer '>Home</h2>
         </div>
         <div>
-            <h2 className='text-gray-950 text-xl font-semibold'>|</h2>
+            <h2 className='text-gray-950 py-2 text-xl font-semibold'>|</h2>
         </div>
         <div>
-            <h2 className='text-gray-950 mx-4 text-xl font-bold hover:-translate-x-4 hover:transition-all duration-1000 cursor-pointer'>Note</h2>
+            <h2 className='text-gray-950 mx-4 py-2 text-xl font-bold hover:-translate-x-4 hover:transition-all duration-1000 cursor-pointer'>Note's</h2>
+        </div>
+        <div>
+          <h2 className=' absolute right-10 top-3'> =#D </h2>
         </div>
     </nav>
   )
